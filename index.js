370735a8cee8517818cf2bf47dd344e19b6d2053
Marcelo Alves <marceloalves_ac@hotmail.com>
2019-12-09 21:39:20 +0000

    auth user

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+// // server.js
+// // load the things we need
+// var express = require('express');
+// var app = express();
+
+// // set the view engine to ejs
+// app.set('view engine', 'ejs');
+
+// // use res.render to load up an ejs view file
+
+// // index page 
+// app.get('/', function(req, res) {
+//     res.render('pages/index');
+// });
+
+// // about page 
+// app.get('/about', function(req, res) {
+//     res.render('pages/about');
+// });
+
+// app.listen(8080);
+// console.log('8080 is the magic port');
+
+
+
+
+
+
 var mysql = require('mysql');
 var express = require('express');
 var session = require('express-session');
@@ -13,6 +41,9 @@ var connection = mysql.createConnection({
 
 var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.use(session({
 	secret: 'secret',
 	resave: true,
@@ -22,8 +53,24 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+// app.get('/', function(req, res) {
+//     // response.sendFile(path.join(__dirname + '/login.html'));
+//     res.render('pages/index');
+// });
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// index page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+// login page 
+app.get('/login', function(req, res) {
+    res.render('pages/login');
 });
 
 app.post('/auth', function(request, response) {
@@ -46,11 +93,23 @@ app.post('/auth', function(request, response) {
 	}
 });
 
+// admin page 
+// app.get('/admin', function(req, res) {
+//     res.render('pages/admin');
+// });
+
 app.get('/home', function(request, response) {
+
 	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
+		// response.send('Welcome back, ' + request.session.username + '!');
+		var userName = request.session.username;
+		response.render('pages/admin', {
+			userName: userName
+		});
+
 	} else {
-		response.send('Please login to view this page!');
+		response.render('pages/login');
+		// response.send('Please login to view this page!');
 	}
 	response.end();
 });
